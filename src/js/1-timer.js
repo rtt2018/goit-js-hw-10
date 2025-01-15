1dcddc9bc769163907b292a288e04ca9196cd918
TarasRogiv <r.taras.t.1@gmail.com>
2025-01-16 01:35:08 +0200

    add countdown

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,6 +4,34 @@ import iziToast from 'izitoast';
 
 let userSelectedDate;
 const startButton = document.querySelector('.button-start');
+const calendarField = document.getElementById('datetime-picker');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+
+startButton.addEventListener('click', startCountDown);
+
+function startCountDown() {
+  startButton.disabled = true;
+  calendarField.disabled = true;
+
+  const playCountDown = setInterval(() => {
+    const timePreiod = userSelectedDate - Date.now();
+    if (timePreiod < 0) {
+      clearInterval(playCountDown);
+      calendarField.disabled = false;
+      return;
+    }
+    const timePreiodArray = convertMs(timePreiod);
+    console.log('playCountDown  timePreiodArray:', timePreiodArray);
+    const { days, hours, minutes, seconds } = timePreiodArray;
+    daysSpan.textContent = addLeadingZero(days);
+    hoursSpan.textContent = addLeadingZero(hours);
+    minutesSpan.textContent = addLeadingZero(minutes);
+    secondsSpan.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
 
 const options = {
   locale: Ukrainian,
@@ -48,6 +76,10 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-function addLeadingZero(value) {}
+function addLeadingZero(value) {
+  return value.toString().length === 1
+    ? value.toString().padStart(2, 0)
+    : value.toString();
+}
 
 const calendar = flatpickr('#datetime-picker', options);
