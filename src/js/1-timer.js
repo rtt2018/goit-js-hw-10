771c203c37c8d1fac40d06c4855da88a9a7cb9dc
TarasRogiv <r.taras.t.1@gmail.com>
2025-01-16 15:10:43 +0200

    fix 6

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -50,7 +50,7 @@ const options = {
         messageColor: '#fff',
         backgroundColor: '#ef4040',
         position: 'topRight',
-        iconUrl: '../img/Group.svg',
+        iconUrl: './img/Group.svg',
         iconColor: '#FAFAFB',
         title: 'Error',
         titleColor: '#fff',
